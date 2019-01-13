206e98449a6e5abacff2d457a3adbd05888820e3
Vadim Demedes <vdemedes@gmail.com>
2019-01-13 18:40:01 +0200

    Bump Node.js support

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ module.exports = options => {
 			if (props.type) {
 				const type = props.type;
 
-				if (availableTypes.indexOf(type) === -1) {
+				if (!availableTypes.includes(type)) {
 					throw new TypeError(`Expected "${key}" to be one of ["string", "boolean", "number", "array"], got ${type}`);
 				}
 
